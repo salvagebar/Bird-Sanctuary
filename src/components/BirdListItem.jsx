7dd9a0a0f9e2ead106e256356d3c1f9aa368ea1e
salvagebar <salvagebar@gmail.com>
2023-08-06 15:00:22 -0400

    setting up components

---
+++ src/components/BirdListItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BirdListItem = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default BirdListItem;
---
+++ src/components/Birds.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Birds = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Birds;
